1c8f091281a7b0465fbd0010bee3d995f1d20c82
danlee2002 <68606021+danlee2002@users.noreply.github.com>
2024-02-27 19:14:48 -0500

    finished operators for dualnumbers

--- DualNumber.py
+++ DualNumber.py
@@ -5,14 +5,24 @@ class Dual:
     def __init__(self, real: float, dual: float):
         self.real = real
         self.dual = dual
-
-    def __add__(self, other):
+    def __add__(self, other) -> Dual:
         return Dual(self.real + other.real, self.dual + other.dual)
-    
-    def __sub__(self, other):
-        return Dual(self.real - other.real, self.dual - other.dual)
-    
-    def __mul__(self, other):
-        return Dual(self.real * other.real, self.real * other.dual + other.real * self.dual)
-    
 
+    def __sub__(self, other) -> Dual:
+        return Dual(self.real - other.real, self.dual - other.dual)
+
+    def __mul__(self, other) -> type[Dual]:
+        return Dual(self.real * other.real, self.real * other.dual + other.real * self.dual)
+
+    def __repr__(self) -> str:
+        return f'real: {self.real}, epsilon: {self.dual}'
+
+    def __truediv__(self, other) -> Dual: 
+        return Dual(self.real/other.real,(self.dual *  other.real - self.real * other.dual)/(other.real**2))
+
+    def __neg__(self) -> Dual:
+        return Dual(-self.real, -self.dual)
+    
+    def __pow__(self, pow):
+        return Dual(self.real**pow, pow* self.dual *  self.real **(pow-1) ) 
+    
---
+++ README.md
@@ -0,0 +1,1 @@
+The following code is an implementation of forward mode autodiff.
